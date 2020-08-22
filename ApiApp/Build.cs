741e806deabc944e41291962f63d5212470fe4e3
skaggs1995 <skaggs1995@gmail.com>
2020-08-22 18:41:32 -0400

    Added Build class to return build value

---
+++ ApiApp/Build.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiApp
+{
+    class Build
+    {
+        public int id { get; set; }
+        public string status { get; set; }
+        public string sourceBranch { get; set; }
+        public string result { get; set; }
+        public string startTime { get; set; }
+        public string finishTime { get; set; }
+    }
+}
--- ApiApp/Program.cs
+++ ApiApp/Program.cs
@@ -13,12 +13,14 @@ namespace ApiApp
     {
         static HttpClient client = new HttpClient();
         const string XApiToken = "4edd1b5cb33b861bc35ddb585ba9d668e0726a15";
+        static List<Build> buildList = new List<Build>();
 
         static void Main(string[] args)
         {
             RunAsync().GetAwaiter().GetResult();
             Console.ReadKey();
         }
+
         public static async Task<List<JsonResult>> getBranch()
         {
             List<JsonResult> branchList = null;
@@ -43,6 +45,25 @@ namespace ApiApp
             if (response.IsSuccessStatusCode)
             {
                 var jsonString = await response.Content.ReadAsStringAsync();
+                var build = JsonConvert.DeserializeObject<Build>(jsonString);
+                buildList.Add(build);
+                Console.Write("Running build {0} on the {1} branch", build.id, build.sourceBranch);
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+        static async void getBuild(string branch)
+        {
+            string url = $"https://api.appcenter.ms/v0.1/apps/skaggs1995-gmail.com/AppUWP/branches/{branch}/builds";
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                
+                var jsonString = await response.Content.ReadAsStringAsync();
+                buildList = JsonConvert.DeserializeObject<List<Build>>(jsonString);
+                Console.WriteLine(buildList.Count);
             }
             else
             {
@@ -62,6 +83,22 @@ namespace ApiApp
             {
                 Console.WriteLine(result.branch.name);
                 runBuild(result.branch.name);
+            }
+            foreach (var result in resultList)
+            {
+                getBuild(result.branch.name);
+            }
+            /*
+             * While the builds aren't completed, search through them
+             */
+            int completedBuilds = 0;
+            int totalBuilds = buildList.Count;
+            while (completedBuilds < totalBuilds)
+            {
+                foreach(Build build in buildList)
+                {
+
+                }
             }
         }
     }
